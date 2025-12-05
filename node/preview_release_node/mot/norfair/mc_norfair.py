ec0734079047f2c38b1b7b440a89d358c65ead3d
KazuhitoTakahashi <shigeki.takahashi.dev@gmail.com>
2025-12-05 17:39:21 +0900

    Numpy2系対応。

--- node/preview_release_node/mot/norfair/mc_norfair.py
+++ node/preview_release_node/mot/norfair/mc_norfair.py
@@ -37,7 +37,7 @@ class MultiClassNorfair(object):
         t_class_ids = []
         for class_id in self.tracker_dict.keys():
             # 対象クラス抽出
-            target_index = np.in1d(class_ids, np.array(int(class_id)))
+            target_index = np.isin(class_ids, np.array(int(class_id)))
 
             if len(target_index) == 0:
                 continue
