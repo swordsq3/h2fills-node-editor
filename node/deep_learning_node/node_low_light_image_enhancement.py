71e59b6c5ed42a3293c45213c3ff1ddc97bb4ae6
KazuhitoTakahashi <shigeki.takahashi.dev@gmail.com>
2025-12-05 18:40:04 +0900

    DeepLearningノード：モデルファイル整理。

--- node/deep_learning_node/node_low_light_image_enhancement.py
+++ node/deep_learning_node/node_low_light_image_enhancement.py
@@ -17,36 +17,32 @@ from node.deep_learning_node.low_light_image_enhancement.AGLLNet.agllnet import 
 
 
 class Node(DpgNodeABC):
-    _ver = '0.0.1'
+    _ver = "0.0.1"
 
-    node_label = 'Low-Light Image Enhancement'
-    node_tag = 'LLIE'
+    node_label = "Low-Light Image Enhancement"
+    node_tag = "LLIE"
 
     _opencv_setting_dict = None
 
     # モデル設定
     _model_class = {
-        'TBEFN(320x180)': TBEFN,
-        'TBEFN(640x360)': TBEFN,
-        'SCI(320x180)': SCI,
-        'SCI(640x360)': SCI,
-        'AGLLNet(256x256)': AGLLNet,
-        'AGLLNet(512x384)': AGLLNet,
+        "TBEFN(320x180)": TBEFN,
+        "TBEFN(640x360)": TBEFN,
+        "SCI(320x180)": SCI,
+        "SCI(640x360)": SCI,
+        "AGLLNet(256x256)": AGLLNet,
+        "AGLLNet(512x384)": AGLLNet,
     }
-    _model_base_path = os.path.dirname(os.path.abspath(__file__)) + '/low_light_image_enhancement/'
+    _model_base_path = (
+        os.path.dirname(os.path.abspath(__file__)) + "/low_light_image_enhancement/"
+    )
     _model_path_setting = {
-        'TBEFN(320x180)':
-        _model_base_path + 'TBEFN/saved_model_180x320/model_float32.onnx',
-        'TBEFN(640x360)':
-        _model_base_path + 'TBEFN/saved_model_360x640/model_float32.onnx',
-        'SCI(320x180)':
-        _model_base_path + 'SCI/sci_180x320/sci_180x320.onnx',
-        'SCI(640x360)':
-        _model_base_path + 'SCI/sci_360x640/sci_360x640.onnx',
-        'AGLLNet(256x256)':
-        _model_base_path + 'AGLLNet/saved_model_256x256/model_float32.onnx',
-        'AGLLNet(512x384)':
-        _model_base_path + 'AGLLNet/saved_model_384x512/model_float32.onnx',
+        "TBEFN(320x180)": _model_base_path + "TBEFN/model/tbefn_180x320.onnx",
+        "TBEFN(640x360)": _model_base_path + "TBEFN/model/tbefn_360x640.onnx",
+        "SCI(320x180)": _model_base_path + "SCI/model/sci_180x320.onnx",
+        "SCI(640x360)": _model_base_path + "SCI/model/sci_360x640.onnx",
+        "AGLLNet(256x256)": _model_base_path + "AGLLNet/model/agllnet_256x256.onnx",
+        "AGLLNet(512x384)": _model_base_path + "AGLLNet/model/agllnet_384x512.onnx",
     }
 
     _model_instance = {}
@@ -63,25 +59,35 @@ class Node(DpgNodeABC):
         callback=None,
     ):
         # タグ名
-        tag_node_name = str(node_id) + ':' + self.node_tag
-        tag_node_input01_name = tag_node_name + ':' + self.TYPE_IMAGE + ':Input01'
-        tag_node_input01_value_name = tag_node_name + ':' + self.TYPE_IMAGE + ':Input01Value'
-        tag_node_input02_name = tag_node_name + ':' + self.TYPE_TEXT + ':Input02'
-        tag_node_input02_value_name = tag_node_name + ':' + self.TYPE_TEXT + ':Input02Value'
-        tag_node_output01_name = tag_node_name + ':' + self.TYPE_IMAGE + ':Output01'
-        tag_node_output01_value_name = tag_node_name + ':' + self.TYPE_IMAGE + ':Output01Value'
-        tag_node_output02_name = tag_node_name + ':' + self.TYPE_TIME_MS + ':Output02'
-        tag_node_output02_value_name = tag_node_name + ':' + self.TYPE_TIME_MS + ':Output02Value'
+        tag_node_name = str(node_id) + ":" + self.node_tag
+        tag_node_input01_name = tag_node_name + ":" + self.TYPE_IMAGE + ":Input01"
+        tag_node_input01_value_name = (
+            tag_node_name + ":" + self.TYPE_IMAGE + ":Input01Value"
+        )
+        tag_node_input02_name = tag_node_name + ":" + self.TYPE_TEXT + ":Input02"
+        tag_node_input02_value_name = (
+            tag_node_name + ":" + self.TYPE_TEXT + ":Input02Value"
+        )
+        tag_node_output01_name = tag_node_name + ":" + self.TYPE_IMAGE + ":Output01"
+        tag_node_output01_value_name = (
+            tag_node_name + ":" + self.TYPE_IMAGE + ":Output01Value"
+        )
+        tag_node_output02_name = tag_node_name + ":" + self.TYPE_TIME_MS + ":Output02"
+        tag_node_output02_value_name = (
+            tag_node_name + ":" + self.TYPE_TIME_MS + ":Output02Value"
+        )
 
-        tag_provider_select_name = tag_node_name + ':' + self.TYPE_TEXT + ':Provider'
-        tag_provider_select_value_name = tag_node_name + ':' + self.TYPE_IMAGE + ':ProviderValue'
+        tag_provider_select_name = tag_node_name + ":" + self.TYPE_TEXT + ":Provider"
+        tag_provider_select_value_name = (
+            tag_node_name + ":" + self.TYPE_IMAGE + ":ProviderValue"
+        )
 
         # OpenCV向け設定
         self._opencv_setting_dict = opencv_setting_dict
-        small_window_w = self._opencv_setting_dict['process_width']
-        small_window_h = self._opencv_setting_dict['process_height']
-        use_pref_counter = self._opencv_setting_dict['use_pref_counter']
-        use_gpu = self._opencv_setting_dict['use_gpu']
+        small_window_w = self._opencv_setting_dict["process_width"]
+        small_window_h = self._opencv_setting_dict["process_height"]
+        use_pref_counter = self._opencv_setting_dict["use_pref_counter"]
+        use_gpu = self._opencv_setting_dict["use_gpu"]
 
         # 初期化用黒画像
         black_image = np.zeros((small_window_w, small_window_h, 3))
@@ -103,30 +109,30 @@ class Node(DpgNodeABC):
 
         # ノード
         with dpg.node(
-                tag=tag_node_name,
-                parent=parent,
-                label=self.node_label,
-                pos=pos,
+            tag=tag_node_name,
+            parent=parent,
+            label=self.node_label,
+            pos=pos,
         ):
             # 入力端子
             with dpg.node_attribute(
-                    tag=tag_node_input01_name,
-                    attribute_type=dpg.mvNode_Attr_Input,
+                tag=tag_node_input01_name,
+                attribute_type=dpg.mvNode_Attr_Input,
             ):
                 dpg.add_text(
                     tag=tag_node_input01_value_name,
-                    default_value='Input BGR image',
+                    default_value="Input BGR image",
                 )
             # 画像
             with dpg.node_attribute(
-                    tag=tag_node_output01_name,
-                    attribute_type=dpg.mvNode_Attr_Output,
+                tag=tag_node_output01_name,
+                attribute_type=dpg.mvNode_Attr_Output,
             ):
                 dpg.add_image(tag_node_output01_value_name)
             # 使用アルゴリズム
             with dpg.node_attribute(
-                    tag=tag_node_input02_name,
-                    attribute_type=dpg.mvNode_Attr_Static,
+                tag=tag_node_input02_name,
+                attribute_type=dpg.mvNode_Attr_Static,
             ):
                 dpg.add_combo(
                     list(self._model_class.keys()),
@@ -135,26 +141,26 @@ class Node(DpgNodeABC):
                     tag=tag_node_input02_value_name,
                 )
             if use_gpu:
-	            # CPU/GPU切り替え
-	            with dpg.node_attribute(
-	                    tag=tag_provider_select_name,
-	                    attribute_type=dpg.mvNode_Attr_Static,
-	            ):
-	                dpg.add_radio_button(
-	                    ("CPU", "GPU"),
-	                    tag=tag_provider_select_value_name,
-	                    default_value='CPU',
-	                    horizontal=True,
-	                )
+                # CPU/GPU切り替え
+                with dpg.node_attribute(
+                    tag=tag_provider_select_name,
+                    attribute_type=dpg.mvNode_Attr_Static,
+                ):
+                    dpg.add_radio_button(
+                        ("CPU", "GPU"),
+                        tag=tag_provider_select_value_name,
+                        default_value="CPU",
+                        horizontal=True,
+                    )
             # 処理時間
             if use_pref_counter:
                 with dpg.node_attribute(
-                        tag=tag_node_output02_name,
-                        attribute_type=dpg.mvNode_Attr_Output,
+                    tag=tag_node_output02_name,
+                    attribute_type=dpg.mvNode_Attr_Output,
                 ):
                     dpg.add_text(
                         tag=tag_node_output02_value_name,
-                        default_value='elapsed time(ms)',
+                        default_value="elapsed time(ms)",
                     )
 
         return tag_node_name
@@ -166,26 +172,28 @@ class Node(DpgNodeABC):
         node_image_dict,
         node_result_dict,
     ):
-        tag_node_name = str(node_id) + ':' + self.node_tag
-        input_value02_tag = tag_node_name + ':' + self.TYPE_TEXT + ':Input02Value'
-        output_value01_tag = tag_node_name + ':' + self.TYPE_IMAGE + ':Output01Value'
-        output_value02_tag = tag_node_name + ':' + self.TYPE_TIME_MS + ':Output02Value'
+        tag_node_name = str(node_id) + ":" + self.node_tag
+        input_value02_tag = tag_node_name + ":" + self.TYPE_TEXT + ":Input02Value"
+        output_value01_tag = tag_node_name + ":" + self.TYPE_IMAGE + ":Output01Value"
+        output_value02_tag = tag_node_name + ":" + self.TYPE_TIME_MS + ":Output02Value"
 
-        tag_provider_select_value_name = tag_node_name + ':' + self.TYPE_IMAGE + ':ProviderValue'
+        tag_provider_select_value_name = (
+            tag_node_name + ":" + self.TYPE_IMAGE + ":ProviderValue"
+        )
 
-        small_window_w = self._opencv_setting_dict['process_width']
-        small_window_h = self._opencv_setting_dict['process_height']
-        use_pref_counter = self._opencv_setting_dict['use_pref_counter']
-        use_gpu = self._opencv_setting_dict['use_gpu']
+        small_window_w = self._opencv_setting_dict["process_width"]
+        small_window_h = self._opencv_setting_dict["process_height"]
+        use_pref_counter = self._opencv_setting_dict["use_pref_counter"]
+        use_gpu = self._opencv_setting_dict["use_gpu"]
 
         # 接続情報確認
-        connection_info_src = ''
+        connection_info_src = ""
         for connection_info in connection_list:
-            connection_type = connection_info[0].split(':')[2]
+            connection_type = connection_info[0].split(":")[2]
             if connection_type == self.TYPE_INT:
                 # 接続タグ取得
-                source_tag = connection_info[0] + 'Value'
-                destination_tag = connection_info[1] + 'Value'
+                source_tag = connection_info[0] + "Value"
+                destination_tag = connection_info[1] + "Value"
                 # 値更新
                 input_value = int(dpg_get_value(source_tag))
                 input_value = max([self._min_val, input_value])
@@ -194,37 +202,37 @@ class Node(DpgNodeABC):
             if connection_type == self.TYPE_IMAGE:
                 # 画像取得元のノード名(ID付き)を取得
                 connection_info_src = connection_info[0]
-                connection_info_src = connection_info_src.split(':')[:2]
-                connection_info_src = ':'.join(connection_info_src)
+                connection_info_src = connection_info_src.split(":")[:2]
+                connection_info_src = ":".join(connection_info_src)
 
         # 画像取得
         frame = node_image_dict.get(connection_info_src, None)
 
         # CPU/GPU選択状態取得
-        provider = 'CPU'
+        provider = "CPU"
         if use_gpu:
-        	provider = dpg_get_value(tag_provider_select_value_name)
+            provider = dpg_get_value(tag_provider_select_value_name)
 
         # モデル情報取得
         model_name = dpg_get_value(input_value02_tag)
         model_path = self._model_path_setting[model_name]
         model_class = self._model_class[model_name]
 
-        model_name_with_provider = model_name + '_' + provider
+        model_name_with_provider = model_name + "_" + provider
 
         # モデル取得
         if frame is not None:
             if model_name_with_provider not in self._model_instance:
-                if provider == 'CPU':
-                    providers = ['CPUExecutionProvider']
-                    self._model_instance[
-                        model_name_with_provider] = model_class(
-                            model_path,
-                            providers=providers,
-                        )
+                if provider == "CPU":
+                    providers = ["CPUExecutionProvider"]
+                    self._model_instance[model_name_with_provider] = model_class(
+                        model_path,
+                        providers=providers,
+                    )
                 else:
-                    self._model_instance[
-                        model_name_with_provider] = model_class(model_path)
+                    self._model_instance[model_name_with_provider] = model_class(
+                        model_path
+                    )
 
         # 計測開始
         if frame is not None and use_pref_counter:
@@ -237,8 +245,7 @@ class Node(DpgNodeABC):
         if frame is not None and use_pref_counter:
             elapsed_time = time.perf_counter() - start_time
             elapsed_time = int(elapsed_time * 1000)
-            dpg_set_value(output_value02_tag,
-                          str(elapsed_time).zfill(4) + 'ms')
+            dpg_set_value(output_value02_tag, str(elapsed_time).zfill(4) + "ms")
 
         # 描画
         if frame is not None:
@@ -255,8 +262,8 @@ class Node(DpgNodeABC):
         pass
 
     def get_setting_dict(self, node_id):
-        tag_node_name = str(node_id) + ':' + self.node_tag
-        input_value02_tag = tag_node_name + ':' + self.TYPE_TEXT + ':Input02Value'
+        tag_node_name = str(node_id) + ":" + self.node_tag
+        input_value02_tag = tag_node_name + ":" + self.TYPE_TEXT + ":Input02Value"
 
         # 選択モデル
         model_name = dpg_get_value(input_value02_tag)
@@ -264,15 +271,15 @@ class Node(DpgNodeABC):
         pos = dpg.get_item_pos(tag_node_name)
 
         setting_dict = {}
-        setting_dict['ver'] = self._ver
-        setting_dict['pos'] = pos
+        setting_dict["ver"] = self._ver
+        setting_dict["pos"] = pos
         setting_dict[input_value02_tag] = model_name
 
         return setting_dict
 
     def set_setting_dict(self, node_id, setting_dict):
-        tag_node_name = str(node_id) + ':' + self.node_tag
-        input_value02_tag = tag_node_name + ':' + self.TYPE_TEXT + ':Input02Value'
+        tag_node_name = str(node_id) + ":" + self.node_tag
+        input_value02_tag = tag_node_name + ":" + self.TYPE_TEXT + ":Input02Value"
 
         model_name = setting_dict[input_value02_tag]
 
